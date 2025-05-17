ad84932c48b84aa79173806f07a955989fa33003
Edu28933 <esolaresv@miumg.edu.gt>
2025-05-17 03:53:11 -0600

    se agregaron stored procedure para mostrar datos en las capas de datos para las tablas empleados, estadisticas y pagos

---
+++ PROYECTO INGE VELOZ/CAPADATOS/cd_04Estadisticas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPADATOS
+{
+    internal class cd_04Estadisticas
+    {
+        cd_Conexion cd_Conexion = new cd_Conexion();
+
+        public DataTable MtMostrarEstadisticas()
+        {
+            string QryMostrarEstadisticas = "usp_estadisticas_mostrar";
+            SqlDataAdapter adapter = new SqlDataAdapter(QryMostrarEstadisticas, cd_Conexion.MtdAbrirConexion());
+            DataTable dtMostrarEstadisticas = new DataTable();
+            adapter.Fill(dtMostrarEstadisticas);
+            cd_Conexion.MtdCerrarConexion();
+            return dtMostrarEstadisticas;
+        }
+
+    }
+}
